21912f1b9e1b7b93a50f2cbd7f25d84df32e0345
ohne <ghdlr785@gmail.com>
2022-07-26 15:50:55 +0900

    초기 커밋.

---
+++ cote38/cote38.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long facto(long long num) {
+    if (num == 1)
+        return 1;
+    return facto(num - 1) * num;
+}
+
+vector<int> solution(int n, long long k) {
+    vector<int> answer, vt;
+    for (int i = 1; i <= n; i++) {
+        vt.push_back(i);
+    }
+    if (k == 1)//k가 1인 경우 답은 vt그대로
+        return vt;
+
+    k -= 1;
+    long long pre = facto(n);
+    long long mod, div;
+
+    while (vt.size() != 1) {
+        pre /= n;
+        mod = k % pre;
+        div = k / pre;
+        answer.push_back(vt[div]);
+        vt.erase(vt.begin() + div);
+        k = mod;
+        n--;
+    }
+    answer.push_back(vt[0]);
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
